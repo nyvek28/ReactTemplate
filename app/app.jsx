0bea05f4671852b843fb781879767a50e90a7221
Kev <kevynquiros@gmail.com>
2016-10-06 23:48:59 -0600

    New boilerplate

--- app/app.jsx
+++ app/app.jsx
@@ -1,19 +1,14 @@
 var React = require('react'),
     ReactDOM = require('react-dom'),
-    Main = require('Main'),
     {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 //Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 //App css
 require('style!css!sass!AppStyles')
 
 ReactDOM.render(
-  <Router history = {hashHistory}>
-    <Route path = "/" component = {Main}>
-    </Route>
-  </Router>,
+  <p>React Template</p>,
   document.getElementById('app')
 );
